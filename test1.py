8742f6e6175314bbcce4bc250755c77864a2c2c5
wiznikvibe <nikhilshetty439@gmail.com>
2023-07-02 10:49:09 +0530

    OOPS Concepts

--- test1.py
+++ test1.py
@@ -1,17 +1,32 @@
 # 1:37:46
 class Person:
-    def __init__(self, name, surname, email_id, year_of_birth):
-        # Self is a pointer which points towards the class 
-        self.name = name 
-        self.surname = surname
-        self.email_id = email_id
-        self.year_of_birth = year_of_birth
+    # def __init__(self, name, surname, email_id, year_of_birth):
+    #     # Self is a pointer which points towards the class 
+    #     # self.name = name 
+    #     # self.surname = surname
+    #     # self.email_id = email_id
+    #     # self.year_of_birth = year_of_birth
     
-    def current_age(self, current_year):
-        return current_year - self.year_of_birth
+    def age(self, current_year,year_of_birth):
+        return current_year - year_of_birth
+    
+    def name(self):
+        name = input("What is you Name: ")
+        return name 
+    
+    def height(self):
+        height = float(input("Enter height in Inches: "))
+        return height
+
+    def weight(self):
+        weight = int(input("What is your weight: "))
+        return weight
+
+         
+
 
 # You can use two init functions but only the first one is considered
-nikhil = Person("Nikhil ","Shetty", "nikhilshetty00@gmail.com",1999)
-print(nikhil.name)
-print(nikhil.name + nikhil.surname)
-print(nikhil.current_age(2023))
+nikhil = Person()
+# print(nikhil.age(current_year=2023,year_of_birth=1999))
+
+print(nikhil.height())
