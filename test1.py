8f705ee8df1d1afa1ccc3bf985845d51e3308547
wiznikvibe <nikhilshetty439@gmail.com>
2023-07-05 20:01:12 +0530

    OOPs Basics

--- test1.py
+++ test1.py
@@ -26,10 +26,10 @@ class Person:
 
 
 # You can use two init functions but only the first one is considered
-nikhil = Person()
-# print(nikhil.age(current_year=2023,year_of_birth=1999))
+# nikhil = Person()
+# # print(nikhil.age(current_year=2023,year_of_birth=1999))
 
-print(nikhil.height())
+# print(nikhil.height())
 
 # Bank Schema using OOPS
 class Bank:
@@ -48,8 +48,40 @@ class Bank:
             self.balance -= amount
 
 
-sbi = Bank("Nikhil", "Shetty")
+# sbi = Bank("Nikhil", "Shetty")
 
-sbi.deposit(50000)
-sbi.withdraw(3500)
-print(sbi.balance)
+# sbi.deposit(50000)
+# sbi.withdraw(3500)
+# print(sbi.balance)
+
+class ToDoList:
+
+    def __init__(self, owner):
+        self.owner = owner 
+        self.tasks = []
+
+    def add_task(self,task):
+        self.tasks.append(task)
+    
+    def complete_task(self, task):
+        for task in self.tasks:
+            self.tasks.remove(task)
+            print(f"Task: {task} Completed")
+    
+    def display_task(self):
+        print(f"The TODO tasks of {self.owner}:")
+        for task in self.tasks:
+            print(task)
+        else: 
+            print("Task was not found")
+
+
+goals = ToDoList("Nikhil")
+
+goals.add_task("Complete the OOPS concepts and implement using it on real world applications")
+goals.add_task("Study the SQL concepts")
+goals.add_task("Build a Interactive Project to showcase your skills")
+goals.add_task("Improve your Resume and Apply for relevant jobs")
+
+goals.complete_task("Complete the OOPS concepts and implement using it on real world applications")
+goals.display_task()
